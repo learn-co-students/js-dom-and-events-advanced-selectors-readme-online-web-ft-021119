5d6f72e73ab9b08ed4c9e8c801acccbbf34de979
ChrstphGrnr <ChristophGruener@me.com>
2019-06-01 16:11:09 -0600

    Done.

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,26 @@
 // declare your functions here...
+
+function paragraphSelector() {
+    let paragraphs = $("p");
+    return paragraphs;
+}
+
+function lastImageSelector() {
+    let image = $('img:last');
+    return image;
+}
+
+function ninjaBabySelector() {
+    let image = $('img#baby-ninja')
+    return image;
+}
+
+function divSelector() {
+    let divs = $('div.pics');
+    return divs;
+}
+
+function firstListItem() {
+    let li = $('ul li:first-child');
+    return li;
+}
